fd5f87b36240b040e02bb5eb86bdd7bb3c1600e6
Tiziano Fagni <tiziano.fagni@isti.cnr.it>
2016-03-09 16:42:35 +0100

    Updated code.

--- src/main/java/it/tizianofagni/sparkboost/DataUtils.java
+++ src/main/java/it/tizianofagni/sparkboost/DataUtils.java
@@ -19,6 +19,27 @@
  * ******************
  */
 
+/*
+ *
+ * ****************
+ * This file is part of nlp4sparkml software package (https://github.com/tizfa/nlp4sparkml).
+ *
+ * Copyright 2016 Tiziano Fagni (tiziano.fagni@isti.cnr.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ******************
+ */
+
 package it.tizianofagni.sparkboost;
 
 
@@ -337,6 +358,8 @@ public class DataUtils {
             throw new NullPointerException("The input model path is 'null'");
 
         List classifiers = sc.objectFile(inputModelPath).collect();
+        if (classifiers.size() == 0)
+            throw new IllegalArgumentException("The specified input model path is not valid. No data available here!");
         BoostClassifier cl = (BoostClassifier) classifiers.get(0);
         return cl;
     }
