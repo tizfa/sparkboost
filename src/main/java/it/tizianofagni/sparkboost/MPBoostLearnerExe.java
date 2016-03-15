faa2e5c2e47d5b9acb4f2fa95a1757e6b698b435
Tiziano Fagni <tiziano.fagni@isti.cnr.it>
2016-03-15 15:54:09 +0100

    Updated code.

--- src/main/java/it/tizianofagni/sparkboost/MPBoostLearnerExe.java
+++ src/main/java/it/tizianofagni/sparkboost/MPBoostLearnerExe.java
@@ -187,6 +187,27 @@
  * ******************
  */
 
+/*
+ *
+ * ****************
+ * This file is part of nlp4sparkml software package (https://github.com/tizfa/nlp4sparkml).
+ *
+ * Copyright 2016 Tiziano Fagni (tiziano.fagni@isti.cnr.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ******************
+ */
+
 package it.tizianofagni.sparkboost;
 
 import org.apache.commons.cli.*;
@@ -251,8 +272,6 @@ public class MPBoostLearnerExe {
         // Create and configure Spark context.
         SparkConf conf = new SparkConf().setAppName("Spark MPBoost learner");
         JavaSparkContext sc = new JavaSparkContext(conf);
-
-        sc.getConf().validateSettings();
 
 
         // Create and configure learner.
--- src/main/java/it/tizianofagni/sparkboost/MpBoostLearner.java
+++ src/main/java/it/tizianofagni/sparkboost/MpBoostLearner.java
@@ -187,6 +187,27 @@
  * ******************
  */
 
+/*
+ *
+ * ****************
+ * This file is part of nlp4sparkml software package (https://github.com/tizfa/nlp4sparkml).
+ *
+ * Copyright 2016 Tiziano Fagni (tiziano.fagni@isti.cnr.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ******************
+ */
+
 package it.tizianofagni.sparkboost;
 
 import org.apache.spark.api.java.JavaRDD;
@@ -238,10 +259,9 @@ public class MpBoostLearner {
             throw new NullPointerException("The SparkContext is 'null'");
         this.sc = sc;
         this.numIterations = 200;
-        this.numDocumentsPartitions = sc.defaultParallelism();
-        this.numFeaturesPartitions = sc.defaultParallelism();
-        this.numLabelsPartitions = sc.defaultParallelism();
-        System.out.println("Docs = " + this.numDocumentsPartitions + ", feats = " + this.numFeaturesPartitions + ", labels = " + this.getNumLabelsPartitions());
+        this.numDocumentsPartitions = -1;
+        this.numFeaturesPartitions = -1;
+        this.numLabelsPartitions = -1;
     }
 
 
@@ -384,10 +404,13 @@ public class MpBoostLearner {
             throw new IllegalArgumentException("The input file is 'null' or empty");
 
         JavaRDD<MultilabelPoint> docs = DataUtils.loadLibSvmFileFormatData(sc, libSvmFile, labels0Based, binaryProblem);
-        this.numDocumentsPartitions = sc.defaultParallelism();
-        this.numFeaturesPartitions = sc.defaultParallelism();
-        this.numLabelsPartitions = sc.defaultParallelism();
-        System.out.println("Docs = " + this.numDocumentsPartitions + ", feats = " + this.numFeaturesPartitions + ", labels = " + this.getNumLabelsPartitions());
+        if (this.numDocumentsPartitions == -1)
+            this.numDocumentsPartitions = sc.defaultParallelism();
+        if (this.numFeaturesPartitions == -1)
+            this.numFeaturesPartitions = sc.defaultParallelism();
+        if (this.numLabelsPartitions == -1)
+            this.numLabelsPartitions = sc.defaultParallelism();
+        Logging.l().info("Docs partitions = " + this.numDocumentsPartitions + ", feats partitions = " + this.numFeaturesPartitions + ", labels partitions = " + this.getNumLabelsPartitions());
         return buildModel(docs);
     }
 
