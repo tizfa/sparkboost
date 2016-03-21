1c20550ab0aaa853884ee2b09d3bba815d9f7be6
Tiziano Fagni <tiziano.fagni@isti.cnr.it>
2016-03-21 12:47:28 +0100

    Fixed Hadoop read file code.

--- src/main/java/it/tizianofagni/sparkboost/DataUtils.java
+++ src/main/java/it/tizianofagni/sparkboost/DataUtils.java
@@ -68,7 +68,7 @@ public class DataUtils {
         ArrayList<MultilabelPoint> points = new ArrayList<>();
         try {
             Path pt = new Path(dataFile);
-            FileSystem fs = FileSystem.get(new Configuration());
+            FileSystem fs = FileSystem.get(pt.toUri(), new Configuration());
             BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
 
             //BufferedReader br = new BufferedReader(new FileReader(dataFile));
