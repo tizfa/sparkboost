992ba18f6ecc0b92c31711d7fc931e33589bb413
Tiziano Fagni <tiziano.fagni@isti.cnr.it>
2016-06-30 18:21:48 +0200

    Updated code.

--- src/main/java/it/tizianofagni/sparkboost/DataUtils.java
+++ src/main/java/it/tizianofagni/sparkboost/DataUtils.java
@@ -108,7 +108,7 @@ public class DataUtils {
             tables.add(new ClassificationPartialResults(id, sb.toString(), ctRes));
             return tables.iterator();
 
-        }, true).persist(StorageLevel.MEMORY_ONLY());
+        }, true).persist(StorageLevel.MEMORY_ONLY_SER());
 
         classifications.saveAsTextFile(outputPath);
 
